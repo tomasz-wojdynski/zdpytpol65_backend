d13d93a95f6b9ace78cd88c007fd98934062a785
jgrynczewski <jgrynczewski@gmail.com>
2023-02-06 21:07:32 +0100

    poprawka na encoding

--- intro/formapp3/views.py
+++ intro/formapp3/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 def register(request):
     if request.method == "GET":
         # Wczytywanie z pliku
-        with open('tasks.txt', 'r') as f:
+        with open('tasks.txt', 'r', encoding='utf-8') as f:
             tasks = f.readlines()
 
         return render(
@@ -21,7 +21,7 @@ def register(request):
 
         # Zapisywanie do pliku
         if task:
-            with open('tasks.txt', 'a') as f:
+            with open('tasks.txt', 'a', encoding='utf-8') as f:
                 f.write(task + "\n")
 
         return redirect('formapp3:register')
