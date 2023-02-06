fc7ec00eda10b956ccebd8045073d81d157a9ef0
jgrynczewski <jgrynczewski@gmail.com>
2023-02-06 19:58:48 +0100

    post form part 2

--- intro/formapp2/views.py
+++ intro/formapp2/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+TASKS = []
 
 
-# Create your views here.
+# Version II
 def register(request):
-    return render(
-        request,
-        'formapp2/register.html'
-    )
+    if request.method == "GET":
+        return render(
+            request,
+            'formapp2/register.html',
+            context={
+                'tasks': TASKS,
+            }
+        )
+
+    elif request.method == "POST":
+        task = request.POST.get('task')
+
+        if task:
+            TASKS.append(task)
+
+        return redirect('formapp2:register')
+        # return redirect('/form2/')
+
+
+# Version I
+# def register(request):
+#     task = request.POST.get('task')
+#
+#     if task:
+#         TASKS.append(task)
+#
+#     return render(
+#         request,
+#         'formapp2/register.html',
+#         context={
+#             'tasks': TASKS,
+#         }
+#     )
+
