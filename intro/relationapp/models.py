5f093699e9e25e1991e57a46683087e32d5dad03
jgrynczewski <jgrynczewski@gmail.com>
2023-02-08 21:04:14 +0100

    relation part I

--- intro/relationapp/models.py
+++ intro/relationapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+# jeden do jeden (one-to-one)
+class Country(models.Model):
+    name = models.CharField(max_length=124)
+    models.OneToOneField("Capital", on_delete=models.CASCADE)
+
+
+class Capital(models.Model):
+    name = models.CharField(max_length=256)
+
