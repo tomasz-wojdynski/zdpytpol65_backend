8ee97a43447d83dfb9d6436261de2fb14974b579
jgrynczewski <jgrynczewski@gmail.com>
2023-02-09 20:16:42 +0100

    orm part III cd

--- intro/relationapp/models.py
+++ intro/relationapp/models.py
@@ -4,9 +4,24 @@ from django.db import models
 # jeden do jeden (one-to-one)
 class Country(models.Model):
     name = models.CharField(max_length=124)
-    models.OneToOneField("Capital", on_delete=models.CASCADE)
+    capital = models.OneToOneField("Capital", on_delete=models.CASCADE)
 
 
 class Capital(models.Model):
     name = models.CharField(max_length=256)
 
+
+# jeden do wielu (one-to-many)
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Framework(models.Model):
+    name = models.CharField(max_length=100)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
