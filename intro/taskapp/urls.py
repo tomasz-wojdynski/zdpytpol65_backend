289f1732b46362d7746bb8f06f56bc1fa6ad9d25
jgrynczewski <jgrynczewski@gmail.com>
2023-02-09 21:27:45 +0100

    aplikacja CRUD

--- intro/taskapp/urls.py
+++ intro/taskapp/urls.py
@@ -5,6 +5,16 @@ from taskapp import views
 app_name = 'taskapp'
 
 urlpatterns = [
-    path('create/', views.create_task_view, name='create-task-view'),
+    # C
+    path('create/', views.task_create_view, name='task-create-view'),
 
+    # R
+    path('', views.task_list_view, name='task-list-view'),
+    path('<int:task_id>/', views.task_detail_view, name='task-detail-view'),
+
+    # U
+    path('<int:task_id>/update/', views.task_update_view, name='task-update-view'),
+
+    # D
+    path('<int:task_id>/delete/', views.task_delete_view, name='task-delete-view')
 ]
--- intro/taskapp/views.py
+++ intro/taskapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from taskapp.models import Task
 
 
-def create_task_view(request):
+def task_create_view(request):
     if request.method == "GET":
         return render(
             request,
@@ -16,4 +16,68 @@ def create_task_view(request):
         if task:
             Task.objects.create(name=task)
 
-        return redirect('taskapp:create-task-view')
+        return redirect('taskapp:task-list-view')
+
+
+def task_list_view(request):
+
+    tasks = Task.objects.all()
+
+    return render(
+        request,
+        'taskapp/tasks.html',
+        context={
+            'tasks': tasks,
+        }
+    )
+
+
+def task_detail_view(request, task_id):
+    task = Task.objects.get(id=task_id)
+
+    return render(
+        request,
+        'taskapp/task.html',
+        context={
+            'task': task
+        }
+    )
+
+
+def task_update_view(request, task_id):
+    task = Task.objects.get(id=task_id)
+
+    if request.method == "GET":
+        return render(
+            request,
+            'taskapp/update_task.html',
+            context={
+                'task': task,
+            }
+        )
+
+    if request.method == "POST":
+        new_task = request.POST.get('task')
+
+        if new_task:
+            task.name = new_task
+            task.save()
+
+        return redirect('taskapp:task-list-view')
+
+
+def task_delete_view(request, task_id):
+    task = Task.objects.get(id=task_id)
+
+    if request.method == "GET":
+        return render(
+            request,
+            'taskapp/delete_task.html',
+            context={
+                'task': task,
+            }
+        )
+
+    if request.method == "POST":
+        task.delete()
+        return redirect('taskapp:task-list-view')
