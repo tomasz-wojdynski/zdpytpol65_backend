6118d356ad397ac83c68d328c06bbaafa42186ce
jgrynczewski <jgrynczewski@gmail.com>
2023-02-21 21:02:31 +0100

    session

--- intro/stateapp/urls.py
+++ intro/stateapp/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('set-cookie/', views.set_cookie, name='set-cookie'),
     path('show-cookies/', views.show_cookies, name='show-cookies'),
     path('delete-cookie/', views.delete_cookie, name='delete-cookie'),
+
+    path('set-session/', views.set_session, name='set-session'),
+    path('show-session/', views.show_session, name='show-session'),
+    path('delete-session/', views.delete_session, name='delete-session'),
 ]
--- intro/stateapp/views.py
+++ intro/stateapp/views.py
@@ -35,3 +35,36 @@ def delete_cookie(request):
     res.delete_cookie('user')
 
     return res
+
+
+def set_session(request):
+
+    request.session['counter'] = 0
+
+    return render(
+        request,
+        'stateapp/session.html'
+    )
+
+
+def show_session(request):
+    if 'counter' in request.session:
+        request.session['counter'] += 1
+
+    return render(
+        request,
+        'stateapp/show_session.html',
+        context={
+            'counter': request.session.get('counter'),
+        }
+    )
+
+
+def delete_session(request):
+    if 'counter' in request.session:
+        del request.session['counter']
+
+    return render(
+        request,
+        'stateapp/session.html'
+    )
